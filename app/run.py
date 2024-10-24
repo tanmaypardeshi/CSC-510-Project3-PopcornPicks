34d77a9f79133ee5849609864951886af406d739
Braden Helmer <bradenhelmeraus@gmail.com>
2024-10-24 14:08:34 -0400

    Syntax error fix

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
 from src import app, socket
 
 if __name__ == '__main__':
-    socket.run(app=app, host="0.0.0.0" debug=True, port=8000, allow_unsafe_werkzeug=True)
+    socket.run(app=app, host="0.0.0.0", debug=True, port=8000, allow_unsafe_werkzeug=True)
